0e2dd49110277569ae0c8bdd48cf5350d4a76878
JoseLeMalin <seb_488@hotmail.fr>
2024-04-22 16:18:46 +0200

    Parent-child update date

--- app/events/[eventId]/edit/EventForm.tsx
+++ app/events/[eventId]/edit/EventForm.tsx
@@ -208,6 +208,7 @@ export default function EventEditForm({ defaultValue }: EventFormEdit) {
                 />
                 <UserCalendar
                   date={eventDate}
+                  updateDate={setEventDate}
                   // events={[
                   //   {
                   //     id: 0,
