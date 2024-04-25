ca434e97da33d54b59ef25c5efb80b84091e810f
JoseLeMalin <seb_488@hotmail.fr>
2024-04-25 17:31:59 +0200

    Working on tests

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -1,16 +1,15 @@
+import { render, screen } from "@testing-library/react";
+// import { StatsCard } from "src/components/decks/StatCard";
+import { StatsCard } from "@/src/components/decks/StatCard";
+import { describe, test } from "vitest";
 
-import { StatsCard } from '@/src/components/decks/StatCard'
-import { render, screen } from '@testing-library/react'
-import { describe, expect, test } from 'vitest'
- 
-describe('Homepage', () => {
-    
-    // test('home component title', () => {
-    //   render(<HomeComponent />)
-    //   expect(screen.getByRole('heading', { level: 1, name: 'Your Activity:' })).toBeDefined()
-    // })
-    test('home component title', () => {
-      render(<StatsCard   title={"the tile"} stat={"the stat"} />)
-      expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
-    })
-})
+// import  from "context"
+describe("Homepage", () => {
+  test("home component title", () => {
+    render(<StatsCard title={"the tile"} stat={"the stat"} />);
+
+    expect(
+      screen.getByRole("heading", { level: 1, name: "Home" }),
+    ).toBeDefined();
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,14 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve("./") }]
+
+  },
   test: {
     environment: "jsdom",
     include: ["./__tests__/*.{test,spec}.?(c|m)[jt]s?(x)"],
